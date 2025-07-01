ddc83653037fee6051aa857b27d4a8683b211316
David <DGrigoryan@korusconsulting.ru>
2025-07-01 20:15:01 +0300

    delete storybook

--- src/shared/config/storybook/StyleDecorator/StyleDecorator.ts
+++
@@ -1,5 +0,0 @@
-import "@/app/styles/index.scss";
-import "@/app/styles/reset.scss";
-import { StoryObj } from "@storybook/react";
-
-export const StyleDecorator = (story: () => StoryObj) => story();
